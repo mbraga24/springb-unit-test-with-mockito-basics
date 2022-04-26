249298d4d0c058d212f0f9c95f63c9127141b75d
mrdbrg <rdg.brg@gmail.com>
2022-04-26 19:50:29 -0400

    add business logic

--- src/main/java/com/dev/unittesting/unittesting/business/ABusinessImpl.java
+++ src/main/java/com/dev/unittesting/unittesting/business/ABusinessImpl.java
@@ -1,5 +1,20 @@
 package com.dev.unittesting.unittesting.business;
 
 public class ABusinessImpl {
+	
+	public int calculateSum(int[] data) {
+		int sum = 0;
+		for (int value:data) {
+			sum += value;
+		}
+		return sum;
+	}
+	
+	//	public int calculateSumFunctional(int[] dataInfo) {
+	//		
+	//		return dataInfo.stream()
+	//		.reduce((a,b) -> a + b);
+	//		.reduce(0, Integer::sum);
+	//	}
 
 }
