f8b503d7e500bce7285f2aaf2206d3cbb166a5a8
mrdbrg <rdg.brg@gmail.com>
2022-04-26 20:22:57 -0400

    ABusinessStubTest

---
+++ src/test/java/com/dev/unittesting/unittesting/business/ABusinessStubTest.java
@@ -0,0 +1,39 @@
+package com.dev.unittesting.unittesting.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ABusinessStubTest {
+
+	@Test
+	public void calculateSum_basic() {
+		ABusinessImpl aBusinessImpl = new ABusinessImpl();
+		
+		int actualResult = aBusinessImpl.calculateSum(new int[] {1,2,3});
+		int expectedResult = 6;
+		
+		assertEquals(expectedResult, actualResult);		
+	}
+	
+	@Test
+	public void calculateSum_emptyArray() {
+		ABusinessImpl aBusinessImpl = new ABusinessImpl();
+		
+		int actualResult = aBusinessImpl.calculateSum(new int[] {});
+		int expectedResult = 0;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void calculateSum_oneValue() {
+		ABusinessImpl aBusinessImpl = new ABusinessImpl();
+		
+		int actualResult = aBusinessImpl.calculateSum(new int[] {1});
+		int expectedResult = 1;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+}
