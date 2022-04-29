be774f6f087b36dbdbfe7328dd44d9a491a55028
mrdbrg <rdg.brg@gmail.com>
2022-04-28 23:44:15 -0400

    spy vs mocking

---
+++ src/test/java/com/dev/unittesting/unittesting/business/SpyingMockTest.java
@@ -0,0 +1,24 @@
+package com.dev.unittesting.unittesting.business;
+
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class SpyingMockTest {
+
+	/*
+	 * A mock does not retain behavior (code) of the original class.
+	 */
+	
+	@Test
+	public void mockingTest() {
+		
+		ArrayList<String> arrayListMock = mock(ArrayList<String>class);
+		
+		System.out.println(arrayListMock.get(0));
+		
+	}
+	
+}
