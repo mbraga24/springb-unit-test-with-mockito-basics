74110d22acf335fe1123e14a67f2ca519957121a
mrdbrg <rdg.brg@gmail.com>
2022-05-21 14:53:01 -0400

    delete comment

--- src/test/java/com/dev/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/dev/unittesting/unittesting/controller/ItemControllerTest.java
@@ -53,38 +53,4 @@ class ItemControllerTest {
 		 * STEP 4: Verify if the result contains the values we want. (This step varies based on each test case)
 		 */
 	}
-
-	
-//	@Test
-//	void test() throws Exception {
-//		/* 
-//		 * STEP 2: Build the request 
-//		 * - Needs a builder
-//		 * - A variable that can store a builder value. (Builds a MockHttpServletRequest)
-//		 * - Use class containing Static factory methods.
-//		 * - Invoke a method that represents your CRUD operation.
-//		 * - What is the Media Type?
-//		 */
-//		RequestBuilder request = MockMvcRequestBuilders
-//				.get("/shop-item")
-//				.accept(MediaType.APPLICATION_JSON);
-//		
-//		/*
-//		 * STEP 3: Execute the request and retrieve the result. 
-//		 * - Use mockMvc instance.
-//		 * - Needs a method to execute (think of a synonym) request. (The return type is ResultActions that allows chaining of further actions)
-//		 * - Needs to initialize a variable to store the returned content. (Provides access to the result of an executed request)
-//		 * - Are we expecting anything from this execution? (Implement Matchers)
-//		 * - Needs to implement an explicit method to return content.
-//		 */
-//		MvcResult result = mockMvc
-//				.perform(request)
-//				.andExpect(MockMvcResultMatchers.status().isOk())
-//				.andExpect(MockMvcResultMatchers.content().json("{\"id\":1,\"name\":\"Sunglasses\",\"price\":119.99,\"quantity\":85}"))
-//				.andReturn();
-//			
-//		/*
-//		 * STEP 4: Verify if the result contains the values we want. (This step varies based on each test case)
-//		 */
-//	}
 }
