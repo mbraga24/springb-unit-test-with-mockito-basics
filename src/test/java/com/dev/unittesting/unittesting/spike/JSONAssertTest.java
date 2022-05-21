60c6cb5ad805ad44073deaf4ed87820b0ddf1982
mrdbrg <rdg.brg@gmail.com>
2022-05-21 14:51:27 -0400

    git revert nightmare - lesson learned

---
+++ src/test/java/com/dev/unittesting/unittesting/spike/JSONAssertTest.java
@@ -0,0 +1,15 @@
+package com.dev.unittesting.unittesting.spike;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JSONAssertTest {
+
+	@Test
+	void test() {
+		fail("Did a git revert on the commit for this class and lost it all! Lesson learned.\n"
+				+ "Sorry, not gonna do it all over again now.");
+	}
+
+}
