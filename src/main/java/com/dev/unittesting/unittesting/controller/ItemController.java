6a0da17c19544884b7fc12cda38eefbe339a1614
mrdbrg <rdg.brg@gmail.com>
2022-06-27 21:16:43 -0400

    add method retrieveAll() to controller

--- src/main/java/com/dev/unittesting/unittesting/controller/ItemController.java
+++ src/main/java/com/dev/unittesting/unittesting/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.dev.unittesting.unittesting.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,11 @@ public class ItemController {
 	@GetMapping("/item-business-sample")
 	public Item retrieveItem() {
 		return businessService.retrieveHardcodedItem();
+	}
+	
+	@GetMapping("/list-all")
+	public List<Item> retrieveAll() {
+		return businessService.retrieveAllItems();
 	}
 	
 }
