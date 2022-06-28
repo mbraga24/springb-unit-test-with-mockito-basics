91a8e114fd8872f57dcce4300fe282242cf30353
mrdbrg <rdg.brg@gmail.com>
2022-06-27 21:18:06 -0400

    add retrieveAllItems method

--- src/main/java/com/dev/unittesting/unittesting/business/ItemBusinessService.java
+++ src/main/java/com/dev/unittesting/unittesting/business/ItemBusinessService.java
@@ -1,14 +1,35 @@
 package com.dev.unittesting.unittesting.business;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.dev.unittesting.unittesting.data.ItemRepository;
+import com.dev.unittesting.unittesting.helper.BusinessHelper;
 import com.dev.unittesting.unittesting.model.Item;
 
 @Component
 public class ItemBusinessService {
 	
+	@Autowired
+	private ItemRepository itemRepository;
+	
 	public Item retrieveHardcodedItem() {
 		return new Item(1, "Iphone 6 Cover - black", 119.95, 85);
 	}
 
+	public List<Item> retrieveAllItems() {
+		List<Item> items = itemRepository.findAll();
+		
+		double newPrice = 0.0;
+		for (Item item:items) {
+			newPrice = BusinessHelper.roundTwoDecimals(20.0, item.getPrice());
+			item.setPrice(newPrice);
+		}
+		return items;
+	}
+	
 }
