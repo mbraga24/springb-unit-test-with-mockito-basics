e93305cbb719853c6a1138cdb9baa7c65d5eef78
mrdbrg <rdg.brg@gmail.com>
2022-07-10 20:00:16 -0400

    delete repository

--- src/main/java/com/dev/unittesting/unittesting/data/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.unittesting.unittesting.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.unittesting.unittesting.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
